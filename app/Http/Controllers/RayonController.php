2fd95409b14fa4fa6f03615bd64184c89872acba
Abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-03-12 00:37:19 +0100

    Implement Product and Rayon resources with controllers, requests, models, policies, factories, migrations, and seeders

---
+++ app/Http/Controllers/RayonController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Rayon;
+use App\Http\Requests\StoreRayonRequest;
+use App\Http\Requests\UpdateRayonRequest;
+
+class RayonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRayonRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Rayon $rayon)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRayonRequest $request, Rayon $rayon)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Rayon $rayon)
+    {
+        //
+    }
+}
